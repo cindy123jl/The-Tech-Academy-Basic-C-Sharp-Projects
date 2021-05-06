bc35a612a0b4869364a11f84724835751890d5cf
Cindy <cindy123jl@gmail.com>
2021-05-06 16:31:21 -0700

    inital-commit

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -5,10 +5,23 @@ namespace ConsoleApp
     class Program
     {
         static void Main(string[] args)
-        {   //asking the user what their favourite color is
+        {
+
+            //assigning i a value of 0
+            int i = 0;
+            while (i < 5) //while i is less than 5 run program
+            {
+                Console.WriteLine(i);//printing out what i is while running the program it should print 0-4
+                i++;//increment by 1
+            }
+
+
+            //asking the user what their favourite color is
             Console.WriteLine("Guess my favourite color: ");
             string color = Console.ReadLine(); //read users input 
             bool GuessedColor = color == "blue"; //data type bool called GuessedColor is assigned a value of color where it equals blue
+
+           
 
             do
             {
@@ -18,7 +31,7 @@ namespace ConsoleApp
                         Console.WriteLine("I like pink but it's not my favourite color!");
                         Console.WriteLine("Guess another color: ");
                         color = Console.ReadLine();//takes in the users input
-                        break; 
+                        break;
                     case "red"://if users answers red it prompts a message and asks them to try again
                         Console.WriteLine("Red is not my favourite color!");
                         Console.WriteLine("Guess another color: ");
@@ -38,13 +51,16 @@ namespace ConsoleApp
                         Console.WriteLine("Guess another color: ");//asks to guess again
                         color = Console.ReadLine();
                         break;
+
+
                 }
             }
 
-            while (!GuessedColor); //program continues as long as Guessed color isn't correctly guessed
+                while (!GuessedColor) ; //program continues as long as Guessed color isn't correctly guessed
 
-            Console.ReadLine();
+                Console.ReadLine();
 
+            }
         }
     }
-}
+
