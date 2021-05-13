9d1131e57daad9da14f9bbd275b7898b55028912
Cindy <cindy123jl@gmail.com>
2021-05-12 19:00:19 -0700

    initial-commit

---
+++ MainMethodAssignment/MainMethodAssignment/Operations.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    public class Operations // our class called Operations which can be called to our Program.cs
+    {
+        public int Methods(int num1) // one of our methods in int 
+        {
+            int total = num1 + 3; // num1 is is what the user put in for their number adding 3 to it and setting the total
+            return total;         // to int and then returning the total so we can then read in our Program.cs
+        }
+
+        public double Methods(double num2)// methods that will take in a double
+        {
+            double total2 = num2 * 3; // the total2 will come out as a double but we will need to cast it into a int and
+            double Total2 = Convert.ToInt32(total2);// we do that here
+            return Total2;//then we return the total2 as a int 
+        }
+
+        public string Methods(string num3)
+        {
+            int Num3 = Convert.ToInt32(num3);
+            string Total3 = Convert.ToInt32(total3);
+            int Total3 = Num3 / 2;
+           // int total3 = Convert.ToInt32(Total3);
+            return Total3;
+        }
+    }
+}
---
+++ MainMethodAssignment/MainMethodAssignment/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class OperationClass
+    {
+        static void Main(string[] args)
+        {
+            Operations OperationsObj = new Operations();
+            Console.WriteLine("Please enter a number: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            int total = OperationsObj.Methods(num1);
+            Console.WriteLine(total);
+
+
+            Console.WriteLine("Please enter a decimal number: ");
+            double num2 = double.Parse(Console.ReadLine());
+
+            double total2 = OperationsObj.Methods(num2);
+            
+            Console.WriteLine(total2);
+            Console.ReadLine();
+
+
+
+            
+        }
+    }
+}
