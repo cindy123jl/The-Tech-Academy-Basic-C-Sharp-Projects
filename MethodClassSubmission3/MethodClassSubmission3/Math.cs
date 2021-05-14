72effc5185dbf0e33155be9dabe76a7b8260e695
Cindy <cindy123jl@gmail.com>
2021-05-13 21:07:09 -0700

    initial-commit

---
+++ MethodClassSubmission3/MethodClassSubmission3/Math.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassSubmission3
+{
+   
+
+    class Math//our class Math
+    {
+        public void MyMethod(out int x, out int y)//name of method is MyMethod void means method doesn't need to return value
+        {
+            x = 5; //assign x a value of 5
+
+            y = 3;//assign y a value of 3
+            
+        }
+
+       
+    }
+}
---
+++ MethodClassSubmission3/MethodClassSubmission3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MethodClassSubmission3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;// num1 is assigned datatype of num1
+            int num2;// num2 is assigned datatype
+
+            Math MathObj = new Math(); //instantiating
+
+            MathObj.MyMethod(out num1, out num2); // num1 is 5 and num2 is 3 from the values we gave them in our MyMethod 
+            Console.WriteLine(num2);//display num2 which is the number3
+
+
+        }
+    }
+}
