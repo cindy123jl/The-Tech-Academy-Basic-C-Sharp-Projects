be28b7a70574bcc072b7b4c45047059106d177fd
Cindy <cindy123jl@gmail.com>
2021-05-14 20:19:15 -0700

    initial-commit

--- ClassSubmissionAssignment/ClassSubmissionAssignment/Operations.cs
+++ ClassSubmissionAssignment/ClassSubmissionAssignment/Operations.cs
@@ -2,17 +2,27 @@
 namespace ClassSubmissionAssignment
 {
     public class Operations
-    {   //our method with arguments
-        public void methodOne(int x, int y, out int total)
+    {   //our method 
+        public void methodOne(out int num1)//void method out int num1 is used to pass argument to a method by reference 
         {
-            
-            total = x / y;//math operation
-          
+            num1 = 8; //num1 is assigned 8
+            Console.WriteLine("Out num1 with the value is by 2: " + num1 / 2);//is shown to the screen and also makes the math operation
+            Console.WriteLine("Enter a number: ");// asks user for input
+            int num2 = Convert.ToInt32(Console.ReadLine());//users input is held in num2
+            Console.WriteLine("Your input divided by 2: " + num2 / 2);//calculate and what is displayed
+        }
+        public void methodTwo(out int num3)//second void method
+        {
+
+            num3 = 2;// num3 has value of 2
         }
 
-        static void methodOne(string z)
+        static class Math// the static class called Math
         {
-            Console.WriteLine(z);
+            public static void methodThree(out int num4)//another void method
+            {
+                num4 = 2; //num4 value 2
+            }
         }
 
        
